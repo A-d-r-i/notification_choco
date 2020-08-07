093c3e143a9f181806ff4c4daf9f2d22d8f7c988
A-d-r-i <charbadrien@hotmail.fr>
2020-08-07 19:06:26 +0200

    Add an upgrade functionnality for check script

--- check_choco.ps1
+++ check_choco.ps1
@@ -1,1 +1,4 @@
-Start-Process powershell -Verb runAs -ArgumentList "choco outdated`n`rpause"
+Start-Process powershell -Verb runAs -ArgumentList "choco outdated`n` 
+Write-Host '----------------'`n`
+Write-Host 'Would you upgrade all the outdated packages? (press enter if you would - quit if you would not)'`n`rpause
+choco upgrade all -y`n`rpause"
